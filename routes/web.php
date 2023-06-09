b3a6a045464ed8b0c3ce429217fcb3c941b3d995
Chardaviski <eduardaleal753@gmail.com>
2023-06-09 18:15:53 -0300

    passando parametros pela url

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,32 @@ Route::match(['get', 'post'], '/match', function(){
 Route::match(['post'], '/post', function(){
     return 'permite apenas acessos definidos';
 });
+
+/**Passando informações por meio da url
+ * na url vamos definir o parametro que vamos passar
+ * faremos isso colcoando uma segunda / após o nome
+ * do na url, abrimos chaves {} e colocamos o parametro
+ * que vamos passar, no nosso caso o id.
+ * Mas para passarmos primeiramente temos que intercepta-lo
+ * para isso colocamos a variável que se refere a essa
+ * informação dentro do parametro da function
+ */
+Route::get('/produto/{id}', function($id_produto){
+    return "O id do produto é:".$id_produto;
+});
+//Exemplo de como vai ficar nossa url http://127.0.0.1:8000/produto/123
+
+/**Passando mais de um parametro na url */
+Route::get('/loja/{id}/{categoria}', function($id_produto, $categoria){
+    return "O id do produto é:".$id_produto."<br>"."Acategoria é:".$categoria;
+});
+//Exemplo de como vai ficar nossa url http://127.0.0.1:8000/loja/123/limpeza
+
+/**Para podermos passar parametros em branco
+ * para situações, por exemplo, onde o usuário pode
+ * ou não preencher tais valores, colocamos uma interrogação
+ * após a variável que pode vir em branco
+*/
+Route::get('/loja/{id}/{categoria?}', function($id_produto, $categoria =''){
+    return "O id do produto é:".$id_produto."<br>"."Acategoria é:".$categoria;
+});
